c27676e6a20c1c6d097a187c6bda22a82e6d6f80
Neeve <133189838+Neeve1@users.noreply.github.com>
2026-07-23 23:01:24 -0700

    Add: valid-palindrome-ii - submission-12

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-12.py
@@ -0,0 +1,44 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        pointer1 = 0
+        pointer2 = len(s) - 1
+
+        while pointer1 < pointer2: 
+        
+            if s[pointer1].lower() != s[pointer2].lower():
+                if self.isPalindrome(pointer1, (pointer2 - 1), s) or self.isPalindrome((pointer1 + 1), pointer2, s):
+                    return True
+
+                else:
+
+                    return False
+
+            else:
+                pointer1 += 1
+                pointer2 -= 1
+
+        return True
+
+    
+    def isPalindrome(self, left, right, s):
+        pointer1 = left
+        pointer2 = right
+
+        while pointer1 < pointer2:
+            if not s[pointer1].isalnum():
+                pointer1 += 1
+                continue 
+
+            if not s[pointer2].isalnum():
+                pointer2 -= 1
+                continue
+        
+            if s[pointer1].lower() != s[pointer2].lower():
+                return False
+
+            else:
+                pointer1 += 1
+                pointer2 -= 1
+
+        return True
+            
